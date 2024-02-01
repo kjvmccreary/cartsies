d3c7adb0b71bc4f09346368fa74394b27003c500
kjvmccreary <kevin@smartgroup-us.com>
2024-02-01 16:19:29 -0600

    migration

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Auction> Auctions { get; set; }
+    // Don't need to specify Item entity in here because it's related to the Auction
+    //  entity. As such, Entity Framework will go ahead and create it anyway. Who knew. 
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,9 +1,16 @@
+using AuctionService.Data;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
